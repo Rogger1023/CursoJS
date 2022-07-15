7f6e49d2980c281dd8e435ab867e41aca789d75b
Rogger Samuel <rogger1023@hotmail.com>
2022-07-15 14:46:40 -0300

    Aula de condições

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`a velocidade do seu carro é ${vel}`);
+if (vel > 60){
+    console.log(`Vc ultrapassou a velocidade!!! esta multado`);
+}
+console.log(`Diriga sempre usando cinto de segurança`);
